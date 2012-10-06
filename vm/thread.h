a9459c4a7fa6a88dcee028aeb23d896d5b8870fd
jiangyingji <jiangyingji1@163.com>
2012-10-06 10:30:09 +0800

    add some fields wo struct thread

--- vm/thread.h
+++ vm/thread.h
@@ -1,20 +1,37 @@
 #ifndef THREAD_H_
 #define THREAD_H_
 
+#define DEFAULT_STACK_SIZE 1024
+
+#define READY 0
+#define RUNNING 1
+
 struct thread{
 	int threadID;
 	int status;
 	int priority;
 	struct stack* pStack;
-	char* pPC;
+	char* PC;
+	char* pName;
+
+	// now we find the children via their father
+	struct thread* pFather;
+	//struct thread* pChildren;
+
+	struct thread* next;
+	struct thread* prev;
 
 };
-struct thread* create(char* pMethod, int stackSize, int priority);
+
+struct thread* create(char* pMethod, int stackSize, int priority, char* PC, char* pName);
+void destroy(struct thread* pthread);
+struct thread* getNextReadyThread();
 void start(struct thread* pthread);
 void stop(struct thread* pthread);
-void destroy(struct thread* pthread);
 
-void pause(struct thread* pthread);
-void continue(struct thread* pthread);
+void suspend(struct thread* pthread);
+void resume(struct thread* pthread);
+
+struct thread* getNextReadyThread();
 
 #endif
